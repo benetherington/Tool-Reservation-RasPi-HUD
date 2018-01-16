4e9306d719ec97ecdafc6586534e18ff3e492eb8
Ben Etherington <ben.j.etherington@gmail.com>
2018-01-16 12:19:13 -0800

    Tidy upcoming_events construction, build current_reservation and next_reservation

--- HUD.rb
+++ HUD.rb
@@ -1,21 +1,25 @@
 from icalendar import Calendar, Event, vDatetime
 from datetime import datetime, timezone
+from tzlocal import get_localzone
 from urllib.request import urlopen
-
 
 url = "https://calendar.google.com/calendar/ical/ideafablabs%40gmail.com/public/basic.ics"
 gcal = Calendar().from_ical(urlopen(url).read().decode('iso-8859-1'))
 
-upcoming_events = []
+Now = datetime.now().astimezone(get_localzone())
+
+events = []
 
 for component in gcal.walk():
         if component.name == "VEVENT" and hasattr(component.get('dtstart').dt, "time"):
-            upcoming_events.append( [component.get('dtstart').dt, component.get('summary')] )
-            # print(component.get('summary'))
-            # print(component.get('dtstart').dt)
-            # print(component.get('dtend'))
-            # print(component.get('dtstamp'))
+            events.append( [component.get('dtstart').dt, component.get('dtend').dt, component.get('summary')] )
 
-sorted_events = sorted(upcoming_events, key= lambda component: component[0])
+sorted_events = sorted(events, key= lambda component: component[0])
+upcoming_events = list( [tstart, tend, description] for tstart, tend, description in sorted_events if tend > datetime.now().astimezone(get_localzone()) )
 
-print(list( time for time, description in sorted_events if time > datetime.now(tz=timezone.utc) ))
+if upcoming_events[0][0] < Now:
+    current_reservation = upcoming_events[0]
+    next_reservation = upcoming_events[1]
+else:
+    current_reservation = None
+    next_reservation = upcoming_events[0]
